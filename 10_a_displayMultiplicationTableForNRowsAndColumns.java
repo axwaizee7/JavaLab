611a6bab4e132e4cc6bcb8cd635ea0483431d1ce
Axwaizee <axwaizee@gmail.com>
2024-02-14 11:45:46 +0000

    added code for while and do while

--- 10_a_displayMultiplicationTableForNRowsAndColumns.java
+++ 10_a_displayMultiplicationTableForNRowsAndColumns.java
@@ -8,10 +8,39 @@ class Multiplicationtable {
         r = s.nextInt();
         System.out.print("Enter the number of table columns: ");
         c = s.nextInt();
+
+
+        // For Loop
         for (ir = 1; ir <= r; ir++) {
             for (ic = 1; ic <= c; ic++)
                 System.out.printf("%4d", ir * ic);
             System.out.println();
         }
+        
+
+        // Do While loop
+        // ir =1;
+        // do {
+        //     ic=1;
+        //     do {
+        //         System.out.printf("%4d", ir * ic);
+        //         ic++;
+        //     } while (ic<=c);
+        //     System.out.println();
+        //     ir++;
+        // } while (ir<=r);
+
+
+        // While Loop
+        // ir =1;
+        // while (ir <=r){
+        //     ic=1;
+        //     while (ic<=c) {
+        //         System.out.printf("%4d", ir * ic);
+        //         ic++;
+        //     }
+        //     System.out.println();
+        //     ir++;
+        // } 
     }
 }
