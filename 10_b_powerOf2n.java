ef906401cd0eedb66e75ad195010931f6f53c192
Axwaizee <axwaizee@gmail.com>
2024-02-14 11:59:47 +0000

    Modified

--- 10_b_powerOf2n.java
+++ 10_b_powerOf2n.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 class PowerOf2n {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter n to find 2^n: ");
+        System.out.print("Enter n to find power of 2: ");
         int n = s.nextInt();
         s.close();
         double r = 1;
-        for (int i = 0; i < n; i++)
-            r *= 2;
-        System.out.printf("2^%d = %.0f", n,  r);
+        for (int i = 0; i <= n; i++) {
+            if (i == 0)
+                System.out.printf("2^%d = 1\n", i);
+            else {
+                r *= 2;
+                System.out.printf("2^%d = %.0f\n", i, r);
+            }
+        }
     }
 }
