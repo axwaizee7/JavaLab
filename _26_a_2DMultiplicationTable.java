2e44598309a61cc1a5d5a8f45840612eab325d31
Axwaizee <axwaizee@gmail.com>
2024-03-27 05:51:14 +0000

    Updated till March 27

---
+++ _26_a_2DMultiplicationTable.java
@@ -0,0 +1,16 @@
+class _26_a_2DMultiplicationTable {
+    public static void main(String[] args) {
+        int row = 10, column = 10;
+        int product[][] = new int[row][column];
+        System.out.println("MULTIPLICATION TABLE");
+        System.out.println(" ");
+        int i, j;
+        for (i = 1; i <= row; i++) {
+            for (j = 1; j <= column; j++) {
+                product[i - 1][j - 1] = i * j;
+                System.out.printf("%4d",(product[i - 1][j - 1]));
+            }
+            System.out.println(" ");
+        }
+    }
+}
